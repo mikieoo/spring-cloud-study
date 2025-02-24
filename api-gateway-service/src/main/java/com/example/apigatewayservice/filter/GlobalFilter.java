9be097ac961eeab98b8bbd9171c06caaec6c4010
miki <jcw2551@naver.com>
2025-02-25 00:15:40 +0900

    gl네êspring cloud gatewayl filter 적용

--- api-gateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -1,5 +1,6 @@
 package com.example.apigatewayservice.filter;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -10,8 +11,8 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-    public CustomFilter() {
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
         super(Config.class);
     }
 
@@ -22,16 +23,25 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Custom PRE filter: request id -> {}", request.getId());
+            log.info("Global Filter baseMessage: -> {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global Filter Start: request id -> {}", request.getId());
+            }
 
             //Custom Post Filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("Custom POST filter: response id -> {}", response.getStatusCode());
+                if (config.isPostLogger()) {
+                    log.info("Global Filter End: response id -> {}", response.getStatusCode());
+                }
             }));
         };
     }
 
+    @Data
     public static class Config {
-        // Put the configuration properties
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
