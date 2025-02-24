d856b574601ee16b8b6fe70c1bf92120a1b88322
miki <jcw2551@naver.com>
2025-02-25 02:19:05 +0900

    spring cloud gateway - load balancer

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+     private final Environment environment;
+     private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return environment.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+}
